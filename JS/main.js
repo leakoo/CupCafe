e5f53578e5625d6187b8ecaff820ef28306506f8
Dan <139128814+leakoo@users.noreply.github.com>
2024-09-05 01:26:01 +0100

    contact page with reset submit + started products page

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
+// Navigation 
 const scroll = () => {
-    document.getElementById("locations-container").scrollIntoView({ behavior: "smooth" });
+    document.getElementById('locations-container').scrollIntoView({ behavior: "smooth" });
 };
 document.querySelector('.btn-1').addEventListener('click', scroll);
 
@@ -7,3 +8,10 @@ const toPage = () => {
     window.location.href = 'products.html';
 };
 document.querySelector('.btn-2').addEventListener('click', toPage);
+
+// Contact Form
+document.getElementById('contact-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    this.reset();
+    alert('Form submitted');
+})
